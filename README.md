6b6c94c39d0f664ebc5404b763dc3e370375ba29
Tim Patton <38817597+pattontim@users.noreply.github.com>
2024-01-14 17:00:21 -0500

    add htm for hosting, script used to gen

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # YouTube.htm
 Backup github page providing a fallback for the SuperMemo YouTube script
+
+Uses youtube.supermemo.org by default and falls back to GitHub if it is down.
+
+## Instructions
+
+SM 18/19:
+
+- Replace YouTube.htm in <SuperMemo_install_folder>/bin with YouTube.htm from this repo.
+
+Fork this repo, change sBaseBackupUrl to host your own. Run script to generate files locally.
+
+## Looking for more? 
+
+Memleak fix, offline video archival and transcript viewer in SuperMemo 
+
+[Supermemo.js](https://github.com/pattontim/supermemo.js)
---
+++ patch_and_dl.ps1
@@ -0,0 +1,54 @@
+# This will completely download the youtube.supermemo.org website and patch it to work.
+
+Invoke-WebRequest -Uri 'https://youtube.supermemo.org/' -OutFile 'index.html'
+
+# Specify the base URL
+$baseUrl = 'https://youtube.supermemo.org/'
+
+# Specify the files to download
+$fileUrls = @(
+    '/images/transparent.png',
+    '/images/icons.png'
+)
+
+# Iterate through each file URL
+foreach ($fileUrl in $fileUrls) {
+    # Construct the full URL
+    $fullUrl = $baseUrl + $fileUrl
+
+    # Get the file name from the URL
+    $fileName = [System.IO.Path]::GetFileName($fileUrl)
+
+    # Construct the local path to save the file
+    $localPath = Join-Path -Path (Resolve-Path .) -ChildPath $fileUrl.TrimStart('/')
+
+    # Create the directory structure if it doesn't exist
+    $directory = [System.IO.Path]::GetDirectoryName($localPath)
+    if (-not (Test-Path -Path $directory -PathType Container)) {
+        New-Item -Path $directory -ItemType Directory -Force
+    }
+
+    # Download the file
+    Invoke-WebRequest -Uri $fullUrl -OutFile $localPath
+
+    Write-Host "Downloaded: $fullUrl to $localPath"
+}
+
+# Specify the file path
+$filePath = "index.html"
+
+# Read the content of the file
+$fileContent = Get-Content -Path $filePath -Raw
+
+# Specify the string to be replaced and the replacement string
+$oldString = "http://supermemory.info/iv/"
+$newString = "/"
+
+# Replace all occurrences of the old string with the new string
+$newContent = $fileContent -replace [regex]::Escape($oldString), $newString
+
+# Write the modified content back to the file, overwriting it
+$newContent | Set-Content -Path $filePath
+
+Write-Host "String replaced in $filePath"
+
