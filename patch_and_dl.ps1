12f339fcf4879685ed6d12f3625b565f9ed2b536
Tim Patton <38817597+pattontim@users.noreply.github.com>
2024-01-21 13:06:05 -0500

    patch load libs over https

--- patch_and_dl.ps1
+++ patch_and_dl.ps1
@@ -1,6 +1,8 @@
 # This will completely download the youtube.supermemo.org website and patch it to work.
 
 Invoke-WebRequest -Uri 'https://youtube.supermemo.org/' -OutFile 'index.html'
+Invoke-WebRequest -Uri 'http://yui.yahooapis.com/combo?2.9.0/build/yahoo-dom-event/yahoo-dom-event.js&2.9.0/build/dragdrop/dragdrop-min.js&2.9.0/build/container/container-min.js' -OutFile 'yui_dom_event_v2.9.0_min.js'
+Invoke-WebRequest -Uri 'http://yui.yahooapis.com/combo?2.8.0r4/build/container/assets/skins/sam/container.css' -OutFile 'yui_container_v2.8.0r4.css'
 
 # Specify the base URL
 $baseUrl = 'https://youtube.supermemo.org/'
@@ -45,10 +47,16 @@ $oldString = "http://supermemory.info/"
 $oldString2 = "/iv/images/"
 $newString = "/"
 $newString2 = "/images/"
+$oldString3 = "http://yui.yahooapis.com/combo?2.9.0/build/yahoo-dom-event/yahoo-dom-event.js&2.9.0/build/dragdrop/dragdrop-min.js&2.9.0/build/container/container-min.js"
+$newString3 = "/yui_dom_event_v2.9.0_min.js"
+$oldString4 = "http://yui.yahooapis.com/combo?2.8.0r4/build/container/assets/skins/sam/container.css"
+$newString4 = "/yui_container_v2.8.0r4.css"
 
 # Replace all occurrences of the old string with the new string
 $newContent = $fileContent -replace [regex]::Escape($oldString), $newString
 $newContent = $newContent -replace [regex]::Escape($oldString2), $newString2
+$newContent = $newContent -replace [regex]::Escape($oldString3), $newString3
+$newContent = $newContent -replace [regex]::Escape($oldString4), $newString4
 
 # Write the modified content back to the file, overwriting it
 $newContent | Set-Content -Path $filePath
